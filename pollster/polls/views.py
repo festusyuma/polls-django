bd61f8e4524a96547ca62c9a4239963612e9bc78
festus agboma <festusyuma@gmail.com>
2021-04-15 22:20:06 +0100

    saafe keeping

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
+from .models import Questions
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_questions = Questions.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions': latest_questions
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
